c4d4f6a95d17c73367e1bbec08ea775509d3afc6
mohamad maheri <mohamadmaheri@yahoo.com>
2019-02-11 21:30:58 +0330

    authoriation completed

---
+++ app/Policies/OrderPolicy.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Policies;
+
+use App\Order;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Log;
+
+class OrderPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function show(User $user,Order $order)
+    {
+
+        if($user->id == $order->user_id)
+        {
+            return true;
+        }
+        else
+        {
+            $roles = $user->roles;
+
+            if(!empty($roles))
+            {
+                foreach ($roles as $role)
+                {
+                    if($role->name == 'manager')
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
