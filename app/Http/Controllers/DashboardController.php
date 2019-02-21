97e359d15b3a3c134e6aa222cfcc6891e815064a
mohamad maheri <mohamadmaheri@yahoo.com>
2019-02-21 15:15:25 +0330

    TODO : view for dashboard/getWorks that 1. showing orders by status 2. can edit each order status 3. submit button for each order_status or for all of them that post to order/{id}/edit like edit order view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,26 +10,44 @@ class DashboardController extends Controller
     public function getWorks()
     {
         $user = auth()->user();
-        $skills = $user->skills;
+        $skills = $user->skills()->get();
 
-        $orders = array();
+        $orders = collect();
         foreach ($skills as $skill)
         {
-            $orders = array_merge($orders, $skill->orders()->get()->toArray());
+            foreach ($skill->orders as $o)
+            {
+                $orders -> push($o);
+            }
         }
-        return $orders;
+
+        $undefined_orders = $this -> filterOrderByStatus('undefined',$orders);
+        $registered_orders = $this -> filterOrderByStatus('registered',$orders);
+        $accepted_orders = $this -> filterOrderByStatus('accepted',$orders);
+        $working_orders = $this -> filterOrderByStatus('working',$orders);
+        $finished_orders = $this -> filterOrderByStatus('finished',$orders);
+        $archived_orders = $this -> filterOrderByStatus('archived',$orders);
+
+        return [
+            'undefined_orders'  =>  $undefined_orders,
+            'registered_orders' =>  $registered_orders,
+            'accepted_orders'   =>  $accepted_orders,
+            'working_orders'    =>  $working_orders,
+            'finished_orders'   =>  $finished_orders,
+            'archived_orders'   =>  $archived_orders
+        ];
     }
 
     public function getMyOrders()
     {
         $orders = Auth::user() -> orders() -> get();
 
-        $undefined_orders = $this->filterOrderByStatus('undefined',$orders);
-        $registered_orders = $this->filterOrderByStatus('registered',$orders);
-        $accepted_orders = $this->filterOrderByStatus('accepted',$orders);
-        $working_orders = $this->filterOrderByStatus('working',$orders);
-        $finished_orders = $this->filterOrderByStatus('finished',$orders);
-        $archived_orders = $this->filterOrderByStatus('archived',$orders);
+        $undefined_orders = $this -> filterOrderByStatus('undefined',$orders);
+        $registered_orders = $this -> filterOrderByStatus('registered',$orders);
+        $accepted_orders = $this -> filterOrderByStatus('accepted',$orders);
+        $working_orders = $this -> filterOrderByStatus('working',$orders);
+        $finished_orders = $this -> filterOrderByStatus('finished',$orders);
+        $archived_orders = $this -> filterOrderByStatus('archived',$orders);
 
         return [
             'undefined_orders'  =>  $undefined_orders,
